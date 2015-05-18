76e6dd0d8488143ae9ac8ebaf65efca77ee1e4e1
jjb <jana@jbee.io>
2015-05-18 12:33:12 -0700

    Update cleanvendor.bat
    added vagrantfiles, .ci and build folders

--- cleanvendor.bat
+++ cleanvendor.bat
@@ -1,12 +1,19 @@
-REM run from the vendor/bin directory
+REM run from the ../vendor directory
+cd vendor
 attrib -s -h -r . /s /d
 del /S .git*
-for /d /r . %d in (Tests) do @if exist "%d" rd /s/q "%d"
-for /d /r . %d in (tests) do @if exist "%d" rd /s/q "%d"
-for /d /r . %d in (test) do @if exist "%d" rd /s/q "%d"
-for /d /r . %d in (Test) do @if exist "%d" rd /s/q "%d"
 del /S composer.*
 del /S *travis*
 del /S LICENSE
 del /S *.md
 del /S *.MD
+del /S php*.x*
+del /S Vagrantfile
+del /S *.markdown
+del /S build.xml
+FOR /D /R %%X IN (test*) DO RD /S /Q "%%X"
+FOR /D /R %%X IN (Test*) DO RD /S /Q "%%X"
+FOR /D /R %%X IN (build) DO RD /S /Q "%%X"
+FOR /D /R %%X IN (*1-test*) DO RD /S /Q "%%X"
+FOR /D /R %%X IN (.ci) DO RD /S /Q "%%X"
+cd ../..
